8bf13f29253d8f0f2e66d937b3c5d9cef3d1c3dc
Colin 't Hart <colin@bandicoot.favoptic.com>
2020-05-04 19:37:25 +0000

    Fix iterator to work on Python 2.7 and 3.x

--- setup.py
+++ setup.py
@@ -472,7 +472,7 @@ def find_mxDateTime():
     candidate_dirs = map(lambda d: os.path.join(d, 'mx', 'DateTime', 'mxDateTime'), sys.path)
     candidate_dirs = filter(lambda d: os.path.exists(os.path.join(d, 'mxDateTime.h')), candidate_dirs)
 
-    return candidate_dirs[0] if candidate_dirs else ''
+    return next(iter(candidate_dirs or ''))
 
 
 # let's start with macro definitions (the ones not already in setup.cfg)
